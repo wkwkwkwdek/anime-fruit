bfb7dcb73041e93cf814344f0c4c8e41cd603611
wkwkwkwdek <mangeakdek01@gmail.com>
2025-07-16 19:56:31 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+-- DISCLAIMER: Hanya untuk pembelajaran. Jangan disalahgunakan!
+
 -- ANTI AFK
 local vu = game:GetService("VirtualUser")
 game:GetService("Players").LocalPlayer.Idled:Connect(function()
@@ -6,30 +8,56 @@ game:GetService("Players").LocalPlayer.Idled:Connect(function()
     vu:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
 end)
 
--- AUTO SPIN FRUIT
+-- AUTO QUEST
 spawn(function()
-    while wait(5) do
-        local remote = game:GetService("ReplicatedStorage"):FindFirstChild("Remotes"):FindFirstChild("SpinFruit")
-        if remote then
-            remote:FireServer()
+    while wait(10) do
+        local questRemote = game:GetService("ReplicatedStorage"):FindFirstChild("Remotes"):FindFirstChild("Quest")
+        if questRemote then
+            questRemote:FireServer("Accept")
         end
     end
 end)
 
--- AUTO ATTACK MOB
+-- AUTO FARM
 spawn(function()
     while wait(1) do
         for _, mob in pairs(workspace.Mobs:GetChildren()) do
-            if mob:FindFirstChild("HumanoidRootPart") and mob:FindFirstChild("Humanoid") and mob.Humanoid.Health > 0 then
+            if mob:FindFirstChild("Humanoid") and mob:FindFirstChild("HumanoidRootPart") and mob.Humanoid.Health > 0 then
                 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = mob.HumanoidRootPart.CFrame * CFrame.new(0, 5, 0)
-                wait(0.2)
+                wait(0.3)
                 game:GetService("ReplicatedStorage").Remotes:FindFirstChild("UseSkill"):FireServer("Z")
             end
         end
     end
 end)
 
--- AUTO COLLECT CHESTS / FRUITS
+-- AUTO DUNGEON MASUK
+spawn(function()
+    while wait(15) do
+        local entrance = workspace:FindFirstChild("DungeonEntrance")
+        if entrance and entrance:FindFirstChildWhichIsA("ProximityPrompt") then
+            fireproximityprompt(entrance:FindFirstChildWhichIsA("ProximityPrompt"))
+        end
+    end
+end)
+
+-- AUTO DUNGEON ATTACK
+spawn(function()
+    while wait(1) do
+        local dungeon = workspace:FindFirstChild("Dungeon")
+        if dungeon then
+            for _, mob in pairs(dungeon:GetDescendants()) do
+                if mob:FindFirstChild("Humanoid") and mob:FindFirstChild("HumanoidRootPart") and mob.Humanoid.Health > 0 then
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = mob.HumanoidRootPart.CFrame * CFrame.new(0, 5, 0)
+                    wait(0.3)
+                    game:GetService("ReplicatedStorage").Remotes:FindFirstChild("UseSkill"):FireServer("Z")
+                end
+            end
+        end
+    end
+end)
+
+-- AUTO CHEST / FRUIT COLLECT
 spawn(function()
     while wait(3) do
         for _, item in pairs(workspace:GetDescendants()) do
@@ -42,4 +70,17 @@ spawn(function()
     end
 end)
 
-print("[Anime Fruit OP Script] Loaded")
+-- AUTO BOSS
+spawn(function()
+    while wait(1) do
+        for _, boss in pairs(workspace.Bosses:GetChildren()) do
+            if boss:FindFirstChild("Humanoid") and boss:FindFirstChild("HumanoidRootPart") and boss.Humanoid.Health > 0 then
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = boss.HumanoidRootPart.CFrame * CFrame.new(0, 5, 0)
+                wait(0.3)
+                game:GetService("ReplicatedStorage").Remotes:FindFirstChild("UseSkill"):FireServer("Z")
+            end
+        end
+    end
+end)
+
+print("[Anime Fruit OP Script Loaded] ✅ AUTO: Quest | Farm | Dungeon | Chest | Boss")
